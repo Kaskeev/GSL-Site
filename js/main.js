66b1a552b1c3056092b5067133697bfe905fd056
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2023-01-12 02:27:08 +0600

    new commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+  $("a[href*=#]").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
+  });
+});
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+AOS.init();
